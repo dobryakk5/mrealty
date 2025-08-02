91a8756d2db48b8904d4adf23dffbe1ad99d048d
dobryakk <dobryakk@gmail.com>
2025-08-03 00:32:43 +0300

    no headless

--- domclick.py
+++ domclick.py
@@ -19,7 +19,7 @@ profile_dir = tempfile.mkdtemp(prefix="chrome-profile-")
 
 # 2) Опции Chrome
 options = webdriver.ChromeOptions()
-options.add_argument("--headless=new")
+#options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-blink-features=AutomationControlled")
