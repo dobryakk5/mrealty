e2e1bd57253b5132aca36fd8aca6e3bce33211fd
dobryakk <dobryakk@gmail.com>
2025-09-17 23:38:25 +0300

    undetect

--- realty_parser_server.py
+++ realty_parser_server.py
@@ -836,13 +836,19 @@ class PersistentAvitoBrowser:
             options.add_argument("--max_old_space_size=512")  # Ограничиваем память V8
             options.add_argument("--window-size=1280,720")
 
-            # User-Agent для обхода блокировок
-            options.add_argument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+            # User-Agent для обхода блокировок (актуальная версия)
+            options.add_argument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.6904.127 Safari/537.36")
 
-            # Отключаем webdriver флаги
+            # Отключаем webdriver флаги и антидетект
             options.add_argument("--disable-blink-features=AutomationControlled")
-            options.add_experimental_option("excludeSwitches", ["enable-automation"])
+            options.add_argument("--disable-features=VizDisplayCompositor")
+            options.add_experimental_option("excludeSwitches", ["enable-automation", "enable-logging"])
             options.add_experimental_option('useAutomationExtension', False)
+            options.add_experimental_option("prefs", {
+                "profile.default_content_setting_values.notifications": 2,
+                "profile.default_content_settings.popups": 0,
+                "profile.managed_default_content_settings.images": 2
+            })
 
             # Chrome binary path
             if os.path.exists("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"):
@@ -854,8 +860,13 @@ class PersistentAvitoBrowser:
             self.driver.set_page_load_timeout(30)
             self.driver.implicitly_wait(5)
 
-            # Убираем webdriver property
-            self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+            # Убираем webdriver property и добавляем антидетект скрипты
+            self.driver.execute_script("""
+                Object.defineProperty(navigator, 'webdriver', {get: () => undefined});
+                Object.defineProperty(navigator, 'plugins', {get: () => [1, 2, 3, 4, 5]});
+                Object.defineProperty(navigator, 'languages', {get: () => ['ru-RU', 'ru', 'en-US', 'en']});
+                window.chrome = {runtime: {}};
+            """)
 
             # Загружаем cookies
             self._load_and_apply_cookies()
